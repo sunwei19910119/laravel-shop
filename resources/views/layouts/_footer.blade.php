9beae5c3d0d99dbcd72d85f344e6e6358ee4cfa5
sunwei19910119 <754293591@qq.com>
2018-10-11 12:09:08 +0800

    基础布局

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由 <a href="http://blog.sunwei1991.cn" target="_blank">Sun</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+    </div>
+</footer>
