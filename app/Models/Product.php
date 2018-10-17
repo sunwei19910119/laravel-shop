bbe45c14a05d52da95a7cb7515e9e71b43562f4b
sunwei19910119 <754293591@qq.com>
2018-10-17 10:34:51 +0800

    用户界面 - 商品列表

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Product extends Model
 {
@@ -18,5 +19,13 @@ class Product extends Model
     //关联商品库存
     public function skus(){
         return $this->hasMany(ProductSku::class);
+    }
+
+    public function getImageUrlAttribute(){
+        //如果image字段本身已经是完整的url就直接返回
+        if(Str::startsWith($this->attributes['image'],['http://','https://'])){
+            return $this->attributes['image'];
+        }
+        return \Storage::disk('public')->url($this->attributes['image']);
     }
 }
--- database/factories/ModlesProductFactory.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Faker\Generator as Faker;
-
-$factory->define(App\Modles\Product::class, function (Faker $faker) {
-    return [
-        //
-    ];
-});
