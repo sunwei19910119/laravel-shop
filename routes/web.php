20e0d752fe0d6b626e03a87bc3868826edadea0e
sunwei19910119 <754293591@qq.com>
2018-10-22 13:00:43 +0800

    创建订单

--- routes/web.php
+++ routes/web.php
@@ -22,12 +22,6 @@ Route::group(['middleware' => 'auth'],function(){
     Route::get('/email_verification/send', 'EmailVerificationController@send')->name('email_verification.send');
 
     Route::group(['middleware' => 'email_verified'],function (){
-//        Route::get('user_addresses','UserAddressesController@index')->name('user_addresses.index');
-//        Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');
-//        Route::post('user_addresses', 'UserAddressesController@store')->name('user_addresses.store');
-//        Route::get('user_addresses/{user_address}', 'UserAddressesController@edit')->name('user_addresses.edit');
-//        Route::put('user_addresses/{user_address}', 'UserAddressesController@update')->name('user_addresses.update');
-//        Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
         Route::resource('user_addresses', 'UserAddressesController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
 
         Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
@@ -36,6 +30,7 @@ Route::group(['middleware' => 'auth'],function(){
         Route::post('cart', 'CartController@add')->name('cart.add');
         Route::get('cart','CartController@index')->name('cart.index');
         Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
+        Route::post('orders', 'OrdersController@store')->name('orders.store');
 
     });
 });
