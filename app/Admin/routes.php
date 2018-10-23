48d611fd68b711793dafd6ed508b287c50e9b1ca
sunwei19910119 <754293591@qq.com>
2018-10-23 14:54:46 +0800

    管理后台 - 订单列表

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -13,4 +13,6 @@ Route::group([
     $router->get('/', 'HomeController@index');
     $router->get('users', 'UsersController@index');
     $router->resource('products', 'ProductsController');
+
+    $router->get('orders', 'OrdersController@index')->name('admin.orders.index');
 });
