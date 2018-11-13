511a1d1d8ad18c51f6c650b5ca66cb51fcd18167
sunwei19910119 <754293591@qq.com>
2018-11-13 14:05:07 +0800

    初始化众筹模型

---
+++ app/Models/CrowdfundingProduct.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CrowdfundingProduct extends Model
+{
+    const STATUS_FUNDING = 'funding';
+    const STATUS_SUCCESS = 'success';
+    const STATUS_FAIL = 'fail';
+
+    public static $statusMap = [
+        self::STATUS_FUNDING => '众筹中',
+        self::STATUS_SUCCESS => '众筹成功',
+        self::STATUS_FAIL => '众筹失败'
+    ];
+
+    protected $fillable = ['total_amount','target_amount','user_count','status','end_at'];
+
+    // end_at 会自动转为 Carbon 类型
+    protected $dates = ['end_at'];
+    // 不需要 created_at 和 updated_at 字段
+    public $timestamps = false;
+
+    public function product(){
+        return $this->belongsTo(Product::class);
+    }
+
+    //众筹进度
+    public function getPercentAttribute(){
+        $value = $this->attributes['total_amount'] / $this->attributes['target_amount'];
+
+        return floatval(number_format($value*100,2,'.',''));
+    }
+}
