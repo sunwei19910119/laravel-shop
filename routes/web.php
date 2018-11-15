843bea24dae6eb2c3ce45209439d98041c4f8552
sunwei19910119 <754293591@qq.com>
2018-11-15 10:13:17 +0800

    创建分期付款

--- routes/web.php
+++ routes/web.php
@@ -44,6 +44,8 @@ Route::group(['middleware' => 'auth'],function(){
         Route::post('crowdfunding_orders', 'OrdersController@crowdfunding')->name('crowdfunding_orders.store');
 
         Route::get('coupon_codes/{code}', 'CouponCodesController@show')->name('coupon_codes.show');
+
+        Route::post('payment/{order}/installment', 'PaymentController@payByInstallment')->name('payment.installment');
     });
 });
 
