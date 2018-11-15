72a755ce5f0e0cbeede861b4d3fbcc4736bb4fce
sunwei19910119 <754293591@qq.com>
2018-11-15 10:22:44 +0800

    前台分期付款列表页

--- routes/web.php
+++ routes/web.php
@@ -46,6 +46,7 @@ Route::group(['middleware' => 'auth'],function(){
         Route::get('coupon_codes/{code}', 'CouponCodesController@show')->name('coupon_codes.show');
 
         Route::post('payment/{order}/installment', 'PaymentController@payByInstallment')->name('payment.installment');
+        Route::get('installments', 'InstallmentsController@index')->name('installments.index');
     });
 });
 
