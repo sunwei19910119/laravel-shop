4639df7d66cdfa3e7e45a570882c41b2e5f7c490
sunwei19910119 <754293591@qq.com>
2018-11-19 16:16:16 +0800

    初始化秒杀模型

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,9 +9,12 @@ class Product extends Model
 {
     const TYPE_NORMAL = 'normal';
     const TYPE_CROWDFUNDING = 'crowdfunding';
+    const TYPE_SECKILL = 'seckill';
+
     public static $typeMap = [
         self::TYPE_NORMAL  => '普通商品',
         self::TYPE_CROWDFUNDING => '众筹商品',
+        self::TYPE_SECKILL => '秒杀商品',
     ];
 
     protected $fillable = [
@@ -26,6 +29,11 @@ class Product extends Model
     //关联商品库存
     public function skus(){
         return $this->hasMany(ProductSku::class);
+    }
+
+    public function seckill()
+    {
+        return $this->hasOne(SeckillProduct::class);
     }
 
     public function crowdfunding()
