efc0513f6c3fa7a589d6ad4b16a8c7a651713889
sunwei19910119 <754293591@qq.com>
2018-11-20 09:00:49 +0800

    秒杀商品详情页

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -10,6 +10,7 @@ use App\Http\Requests\Admin\HandleRefundRequest;
 use App\Http\Requests\ApplyRefundRequest;
 use App\Http\Requests\CrowdFundingOrderRequest;
 use App\Http\Requests\OrderRequest;
+use App\Http\Requests\SeckillOrderRequest;
 use App\Http\Requests\SendReviewRequest;
 use App\Jobs\CloseOrder;
 use App\Models\CouponCode;
@@ -154,5 +155,14 @@ class OrdersController extends Controller
         $address = UserAddress::find($request->input('address_id'));
         $amount = $request->input('amount');
         return $orderService->crowdfunding($user,$address,$sku,$amount);
+    }
+
+    public function seckill(SeckillOrderRequest $request, OrderService $orderService)
+    {
+        $user    = $request->user();
+        $address = UserAddress::find($request->input('address_id'));
+        $sku     = ProductSku::find($request->input('sku_id'));
+
+        return $orderService->seckill($user, $address, $sku);
     }
 }
